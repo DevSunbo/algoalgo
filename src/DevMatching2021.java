3fd09177257c5047da6b1e8b850c3320c9a6d49a
Sim Sunbo <cowwhite1008@gmail.com>
2022-03-18 22:29:06 +0900

    O(n^2) 이라서 나중에 리팩토링 해야함

--- src/DevMatching2021.java
+++ src/DevMatching2021.java
@@ -11,7 +11,7 @@ public class DevMatching2021 {
     public static int[] solution(int[] lottos, int[] win_nums) {
         Arrays.sort(lottos);
         int[] temp = new int[lottos.length];
-        int min= 1, max=1;
+        int min= 7, max=7; // 0개와 1개는 모두 6등이기 때문
         for(int i = 0;i<lottos.length;++i){
             temp[i] = lottos[i];
         }
@@ -20,18 +20,22 @@ public class DevMatching2021 {
         for(int n : lottos){
             if(n == 0){
                 // 아무 번호나 가능
-                ++max;
+                --min;
                 continue;
             }
             for(int w : win_nums){
                 if(n == w){
                     // 로또 번호가 존재한다면
-                    ++min;
-                    ++max;
+                    --min;
+                    --max;
                 }
 
             }
         }
+        if(min < 1) min = 1; // 다 맞으면 1등, 0개 또는 1개 맞으면 6등 케어
+        if(max < 1) max = 1;
+        if(min > 6) min = 6;
+        if(max > 6) max = 6;
 
         int[] answer = {min, max};
         return answer;
