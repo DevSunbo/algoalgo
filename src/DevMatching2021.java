13cb304d620cbd273f400a46fe42e468aadff140
Sim Sunbo <cowwhite1008@gmail.com>
2022-03-18 22:06:10 +0900

    DevMatching 2021

--- src/DevMatching2021.java
+++ src/DevMatching2021.java
@@ -3,20 +3,37 @@ package src;
 import java.util.*;
 
 public class DevMatching2021 {
-    public int[] solution(int[] lottos, int[] win_nums) {
+    public static void main(String[] args) {
+        int[] lottos =  {44, 1, 0, 0, 31, 25};
+        int[] win_nums = {31, 10, 45, 1, 6, 19};
+        solution(lottos, win_nums);
+    }
+    public static int[] solution(int[] lottos, int[] win_nums) {
         Arrays.sort(lottos);
         int[] temp = new int[lottos.length];
+        int min= 1, max=1;
         for(int i = 0;i<lottos.length;++i){
             temp[i] = lottos[i];
         }
         lottos = temp; // Collection.reverse 안쓰고 내림차순
 
         for(int n : lottos){
-            win_nums.stream().fi
+            if(n == 0){
+                // 아무 번호나 가능
+                ++max;
+                continue;
+            }
+            for(int w : win_nums){
+                if(n == w){
+                    // 로또 번호가 존재한다면
+                    ++min;
+                    ++max;
+                }
+
+            }
         }
 
-
-        int[] answer = {};
+        int[] answer = {min, max};
         return answer;
     }
 }
