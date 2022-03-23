9a210e74dc9d50ccc0328cf26690be23be1d977f
Sim Sunbo <cowwhite1008@gmail.com>
2022-03-23 21:53:27 +0900

    3,4 단계 수정ㅑㅜㅎ

--- src/KAKAONewID.java
+++ src/KAKAONewID.java
@@ -8,7 +8,11 @@ import java.util.regex.*;
  * 단, 마침표(.)는 처음과 끝에 사용할 수 없으며 또한 연속으로 사용할 수 없습니다.
  */
 public class KAKAONewID {
-    public String solution(String new_id) {
+    public static void main(String[] args) {
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        solution(new_id);
+    }
+    public static String solution(String new_id) {
         String answer = "";
         String patternLength = "^.\\S{3,15}$"; // 공백 없이 3~15글자
         String pattern = "^[a-z0-9-_]*[a-z0-9-_.]?[a-z0-9-_]*$"; //아이디는 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.) 문자만 사용할 수 있습니다. & 마침표(.)는 처음과 끝에 사용할 수 없으며 또한 연속으로 사용할 수 없습니다.
@@ -26,7 +30,7 @@ public class KAKAONewID {
             new_id = new_id.toLowerCase(); // 1단계 모두 소문자로 만들기
             String[] str = new_id.split("");
             String str2 = "";
-            if(str[str.length-1].equals(".")) str[str.length-1] = "X"; // 4단계 마침표(.)가 처음이나 끝에 위치한다면 제거합니다. X로 바꾸고 아래 for문 2단계 실행에서 제거
+
             boolean doubleDot = false;
             for(int i =0;i<str.length;++i){
                 Matcher matcher = patternCheck.matcher(str[i]);
