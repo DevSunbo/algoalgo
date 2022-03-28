b457c9fb87e66393932cc7df8c692503909ba067
Sim Sunbo <cowwhite1008@gmail.com>
2022-03-28 22:14:04 +0900

    인테리어로 인해 쉬어가는길

--- src/KAKAONewID.java
+++ src/KAKAONewID.java
@@ -61,6 +61,7 @@ public class KAKAONewID {
             //TODO: 내일은 꼭 마무리 한다 - 03.25
             //TODO: 내일은 꼭 마무리 한다 - 03.26
             //TODO: 내일은 꼭 마무리 한다 - 03.27
+            //TODO: 집 인테리어로 인해 쉬어가는 길 - 03.28
             str = str2.split(""); // 2단계를 거친 String 으로 다시 만든다
             if(str.length == 0){
                 //5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
